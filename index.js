ee6875d90b89e34d6b16ced993c21be293e14a1f
gabriell122 <gabriell.haines@etec.sp.gov.br>
2024-10-22 15:03:58 -0300

    bot introduzido

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
-//Define a vez do jogador TRUE jogador 1 FALSE jogador 2
-let player = true;
-
-//Gardar as jogadas dos jogadores
-let player1 = [];
-let player2 = [];
 
 //Historico de quem venceu
 let historic = [];
+
+var ganhador;
+
+//Botões do jogo
+const buttons = document.getElementsByClassName("buttonGame");
+
+//Div do placar
+const dPlacar = document.getElementsByClassName("dPlacar");
 
 //Combinações que ganha o jogo
 const win = [
@@ -20,42 +22,94 @@ const win = [
     [3,5,7]
 ];
 
-//Botões do jogo
-const buttons = document.getElementsByClassName("buttonGame");
+// JOGADAS
+var playerBot = [];
+var player = [];
 
-//Div do placar
-const dPlacar = document.getElementsByClassName("dPlacar");
+// Valida a Jogada
+const Validar = (e)=>{
+    // Se a Jogada ja tiver sido feita retorna true para Jogada Invalida e false para jogada vailida
+    if (playerBot.includes(e) || player.includes(e) || e > 9 || e < 1) {
+        //Erro
+        return true
+    } else {
+        //Tudo Ok
+        return false            
+    }
+}
 
-//Pega o botão jogado
-const Press = (buttonPress) =>{
 
-    //Seleciona o botão
-    const button = buttons[buttonPress - 1];
-    
-    //Desabilita o botão
+// Faz uma Jogada Random
+const JogadaRandom = ()=>{
+    var jogado;
+    // Roda o loop até o bot achar uma jogada valida
+    do {
+        // Sorteia um número aleatorio que ainda não tenha sido jogado pelo Jogador ou pelo Bot
+        jogado =  Math.floor(Math.random()* 9 ) + 1;
+    } while (Validar(jogado) );
+    // Guarda a jogada do bot
+    playerBot.push(jogado);
+
+    //Altera o estilo do Button
+    const button = buttons[jogado - 1];
     button.disabled = true;
+    button.value = "O";
+    button.style.color = "#6080F0";
 
-    //
-    if (player) {
+    //Seta que é jogada do bot
+    ganhador = false;
+
+    //Verifica se o Bot Ganhou
+    const res = Winer(playerBot)
+    if (res) {
+        console.log("Jogador 2 ganhou");
+    }
+}
+
+// Função Jogar
+const Jogar = (jogada) =>{
+    // Vê se a jogada é valida
+    if(Validar(jogada)){
+        console.log("Jogada Invalida");
+    }else{
+
+        //Altera o estilo do Button
+        const button = buttons[jogada - 1];
+        button.disabled = true;
         button.value = "X";
         button.style.color = "#9F3030";
-        player1.push(buttonPress);
-        const res = Winer(player1)
+    
+        // Armazena a jogada do jogador
+        player.push(jogada);
+
+        //Seta qyue é a jogada do jogador
+        ganhador = true;
+
+        //Verifica se o Player Ganhou
+        const res = Winer(player)
         if (res) {
             console.log("Jogador 1 ganhou");
         }
-        player = !player;
-    } else {
-        button.value = "O";
-        button.style.color = "#6080F0";
-        player2.push(buttonPress);
-        const res = Winer(player2)
-        if (res) {
-            console.log("Jogador 2 ganhou");
+
+        // Faz a Jogada do Bot
+        if (player.length + playerBot.length  <= 8) {
+            JogadaRandom();
         }
-        player = !player;
+
+        //LOGS
+        console.log("Jogue Denovo");
+        console.log("Jogador: ", player);
+        console.log("Bot: ", playerBot);
+        console.log(historic);
+        
     }
 }
+
+
+
+
+
+
 
 const Winer = (jogador)=>{
     for (let i = 0; i < win.length; i++) {
@@ -68,7 +122,7 @@ const Winer = (jogador)=>{
             }
         }
         if (victory) {
-            historic.push(player ? 1 : 2);
+            historic.push(ganhador ? 1 : 2);
             const win1 = historic.filter(e => e === 1).length;
             const win2 = historic.length - win1;
             dPlacar[0].innerHTML = `${win1} X ${win2}`; 
@@ -81,8 +135,8 @@ const Winer = (jogador)=>{
 }
 
 const NewGame = () =>{
-    player1 = [];
-    player2 = [];
+    player = [];
+    playerBot = [];
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].disabled = false;
         buttons[i].value = "";
